f90cbeba305cf5b7769036b36b9a1c2b8daa41ee
Александр Орешкин <oreshkin.dev@outlook.com>
2024-04-23 14:19:29 +0300

    определил структуру пользователя и ролей / использую uuid как защита от подмены роли

--- internal/database/postgres/migration.go
+++ internal/database/postgres/migration.go
@@ -7,10 +7,11 @@ import (
 func Migrate(database *Postgres) error {
 	tables := []interface{}{
 		&entity.User{},
+		&entity.AccessLevel{},
 	}
 
 	// Use it for development only
-	if err := database.Exec("DO $$DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE'; END LOOP; END;$$").Error; err != nil {
+	if err := DropTables(database); err != nil {
 		return err
 	}
 
@@ -18,9 +19,48 @@ func Migrate(database *Postgres) error {
 		return err
 	}
 
-	// if err := author.SeedUser(database, config); err != nil {
-	// 	return err
-	// }
+	if err := SeedAccessLevel(database); err != nil {
+		return err
+	}
+
+	if err := SeedUser(database); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func DropTables(database *Postgres) error {
+	return database.Exec("DO $$DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE'; END LOOP; END;$$").Error
+}
+
+func SeedAccessLevel(database *Postgres) error {
+	roles := []entity.AccessLevel{
+		{
+			Title: "Администратор",
+		},
+		{
+			Title: "Менеджер",
+		},
+	}
+
+	return database.Create(&roles).Error
+}
+
+func SeedUser(database *Postgres) error {
+	var accessLevelID string
+
+	if err := database.Model(&entity.AccessLevel{}).Select("id").Where("title = ?", "Администратор").First(&accessLevelID).Error; err != nil {
+		return err
+	}
+
+	user := []entity.User{
+		{
+			Email:         "oreshkin.dev@outlook.com",
+			Password:      "qwerty",
+			AccessLevelID: accessLevelID,
+		},
+	}
+
+	return database.Create(&user).Error
 }
--- internal/user/entity/entity.go
+++ internal/user/entity/entity.go
@@ -1,7 +1,15 @@
 package entity
 
 type User struct {
-	ID    uint64 `json:"id"`
+	ID            string `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
+	Email         string `json:"email" gorm:"unique"`
+	Password      string `json:"password,omitempty"`
+	AccessLevelID string `json:"access_level_id" gorm:"type:uuid"`
+	AccessToken   string `json:"access_token" gorm:"default:null"`
+}
+
+type AccessLevel struct {
+	ID    string `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
 	Title string `json:"title" gorm:"unique"`
 }
 
